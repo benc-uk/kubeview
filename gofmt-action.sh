ab8c7be939f7acffb6b22d6175c9dfbf4716d38f
Ben Coleman <benc.uk@gmail.com>
2020-03-13 13:28:12 +0000

    more gofmt fun

--- gofmt-action.sh
+++ gofmt-action.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-out=$(gofmt -d $1)
+out=$(gofmt -l $1)
 linecount=$(echo "$out" | wc -l)
 
-if (( $linecount > 1 )); then
-  echo "::error::$out"
+if (( $linecount > 0 )); then
+  while IFS= read -r line; do
+    echo "::error::gofmt error in file: $line check logs for details"
+  done <<< "$out"
+
+  # Print actual diff and error details
+  gofmt -d $1
+
   exit 1
 fi
