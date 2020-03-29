90d1711daeff939fda638d00960e440d9ff8d98d
Ben Coleman <benc.uk@gmail.com>
2020-03-29 16:11:20 +0100

    CI linting fixes

--- web/client/src/App.vue
+++ web/client/src/App.vue
@@ -112,7 +112,7 @@ export default {
     conf = await this.apiGetConfig()
 
     this.namespace = 'default'
-    this.autoRefresh = 10
+    this.auto_Refresh = 10
 
     if (conf && conf.NamespaceScope) {
       // Asterisk is default behaviour meaning users can pick any NS
