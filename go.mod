589fbd09b88613b6c891fac3a3567a5fc6c66e49
Ben Coleman <benc.uk@gmail.com>
2021-01-23 18:15:22 +0000

    Switch to new linter & formatters

--- go.mod
+++ go.mod
@@ -4,8 +4,18 @@ go 1.15
 
 require (
 	github.com/benc-uk/go-starter v1.0.0
+	github.com/google/go-cmp v0.5.4 // indirect
 	github.com/gorilla/mux v1.8.0
 	github.com/joho/godotenv v1.3.0
+	github.com/kr/text v0.2.0 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/stretchr/testify v1.6.1 // indirect
+	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
+	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4 // indirect
+	golang.org/x/text v0.3.4 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	k8s.io/api v0.19.0
 	k8s.io/apimachinery v0.19.0
 	k8s.io/client-go v0.19.0
--- web/client/.eslintrc.js
+++ web/client/.eslintrc.js
@@ -2,45 +2,24 @@ module.exports = {
   root: true,
   env: {
     node: true,
-    es6: true
+    es6: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:vue/recommended'
-  ],
+  extends: ['eslint:recommended', 'plugin:vue/recommended'],
   rules: {
     // Errors & best practices
     'no-var': 'error',
     'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-unused-vars': ['error', { 'argsIgnorePattern': 'next|res|req' }],
-    'curly': 'error',
-
-    // ES6
-    'arrow-spacing': 'error',
-    'arrow-parens': 'error',
-
-    // Style
-    'brace-style': ['error', '1tbs', { 'allowSingleLine': true }],
-    'semi': ['error', 'never'],
-    'indent': ['error', 2],
-    'quotes': ['error', 'single'],
-    'camelcase': 'error',
-
-    // Spacing
-    'func-call-spacing': 'error',
-    'block-spacing': 'error',
-    'object-curly-spacing': ['error', 'always'],
-    'no-trailing-spaces': 'error',
-    'no-whitespace-before-property': 'error',
-    'comma-spacing': 'error',
-    'keyword-spacing': 'error',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next|res|req' }],
+    curly: 'error',
 
     // Vue
     'vue/html-closing-bracket-newline': 'off',
-    'vue/max-attributes-per-line': 'off'
+    'vue/max-attributes-per-line': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/html-self-closing': 'off',
   },
   parserOptions: {
-    parser: 'babel-eslint'
-  }
+    parser: 'babel-eslint',
+  },
 }
