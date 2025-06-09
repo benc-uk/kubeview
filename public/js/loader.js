c1421f87c6f172102912950b06b2a05aaf913217
Ben Coleman <benc.uk@gmail.com>
2025-06-09 18:53:15 +0100

    ADD: Implement dynamic loading of HTML fragments for better code structure

---
+++ public/js/loader.js
@@ -0,0 +1,20 @@
+// ================================================================
+// Small JavaScript loader for dynamic HTML components & fragments
+// ================================================================
+
+const PATH = 'public/fragments'
+
+window.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('div[data-fragment]').forEach(async (el) => {
+    const frag = el.getAttribute('data-fragment')
+    if (frag) {
+      const res = await fetch(`${PATH}/${frag}.html`)
+      if (!res.ok) {
+        console.error(`Failed to load HTML fragment: ${frag}`, res.statusText)
+        return
+      }
+      const html = await res.text()
+      el.innerHTML = html
+    }
+  })
+})
