e2c1b096512f54ae39237e61379ec5125b8abd53
Ben Coleman <benc.uk@gmail.com>
2025-07-19 10:29:59 +0100

    Add event handlers for configmaps and secrets in Kubernetes service

--- server/services/kubernetes.go
+++ server/services/kubernetes.go
@@ -202,6 +202,14 @@ func NewKubernetes(sseBroker *sse.Broker[KubeEvent], singleNamespace string) (*K
 			AddEventHandler(getHandlerFuncs(sseBroker))
 	}
 
+	_, _ = factory.ForResource(schema.GroupVersionResource{Group: "", Version: "v1", Resource: "configmaps"}).
+		Informer().
+		AddEventHandler(getHandlerFuncs(sseBroker))
+
+	_, _ = factory.ForResource(schema.GroupVersionResource{Group: "", Version: "v1", Resource: "secrets"}).
+		Informer().
+		AddEventHandler(getHandlerFuncs(sseBroker))
+
 	factory.Start(context.Background().Done())
 	factory.WaitForCacheSync(context.Background().Done())
 
